3c3367a468c6d1f59794d3559b05964e0acf011c
Peter Angusa <peter.angusa@outlook.com>
2019-03-31 18:32:29 +0300

    displays results

--- js/scripts.js
+++ js/scripts.js
@@ -2,170 +2,79 @@ $(document).ready(function() {
  $("form#quiz").submit(function(event) {
    event.preventDefault();
 
-   var name = document.getElementById('name').value
-   var date = document.getElementById('date').value
-
+   var name = $("input#name").val();
+   var date = $("input#date").val();
+   var weather = $("[name=weather]:checked").val();
+   var location = $("[name=location]:checked").val();
+   var city = $("[name=city]:checked").val();
+   var active = $("[name=active]:checked").val();
+   var cost = $("[name=cost]:checked").val();
    var total1 = 0;
    var total2 = 0;
    var total3 = 0;
+debugger;
 // question 1
-   function one(weather) {
-     if (document.getElementById('weather1').checked) {
-       total1+=10;
-       return total1;
-     }
-     else if (document.getElementById('weather2').checked) {
-       total2+=5;
-       return total2;
-     }
-     else {
-        total3+=3;
-        return total3;
-     }
- }
-
- function two(location) {
-   if (document.getElementById('location1').checked) {
-     total1+=10;
-     return total1;
+   if (weather === "b") {
+     total1 += 10;
+   } else if (weather === "a") {
+     total2 += 5;
+   } else if (weather === "c"){
+     total3 += 1;
    }
-   else if (document.getElementById('location2').checked) {
-     total2+=5;
-     return total2;
-   }
-   else {
-      total3+=3;
-      return total3;
-   }
- }
- function three(city) {
-   if (document.getElementById('city1').checked) {
-     total1+=10;
-     return total1;
-   }
-   else if (document.getElementById('city2').checked) {
-     total2+=5;
-     return total2;
-   }
-   else {
-      total3+=3;
-      return total3;
-   }
- }
- function four(self) {
-   if (document.getElementById('self1').checked) {
-     total1+=10;
-     return total1;
-   }
-   else if (document.getElementById('self2').checked) {
-     total2+=5;
-     return total2;
-   }
-   else {
-      total3+=3;
-      return total3;
-   }
- }
- function five(money) {
-   if (document.getElementById('money1').checked) {
-     total1+=10;
-     return total1;
-   }
-   else if (document.getElementById('money2').checked) {
-     total2+=5;
-     return total2;
-   }
-   else {
-      total3+=3;
-      return total3;
-   }
- }
-var one=one()
-var two=two()
-var three=three()
-var four=four()
-var five=five()
-var answer=[one,two,three,four,five]
-alert (answer)
-
-
-
 
 // question 2
-  /* function location () {
-     if (location === "b") {
+   if (location === "b") {
       total1 += 5;
-      return total1;
     } else if (location === "c") {
       total2 += 1;
-      return total2;
     } else if (location === "a") {
       total3 += 10;
-      return total3;
     }
-  }
 
 // question 3
-   function city() {
-     if (city === "c") {
+   if (city === "c") {
       total1 += 1;
-      return total1;
     } else if (city === "a") {
       total2 += 10;
-      return total2;
     } else if (city === "b") {
       total3 += 5;
-      return total3;
     }
-  }
 
 // question 4
-    function self() {
-      if (active === "c") {
+    if (active === "c") {
        total1 += 1;
-       return total1;
      } else if (active === "b") {
        total2 += 5;
-       return total2;
      } else if (active === "a") {
        total3 += 10;
-       return total3;
      }
-   }
 
 // question 5
-   function cost() {
-     if (cost === "a") {
+   if (cost === "a") {
       total1 += 10;
-      return total1;
     } else if (cost === "c") {
       total2 += 1;
-      return total2;
     } else if (cost === "b") {
       total3 += 5;
-      return total3;
     }
-  }*/
 
-
-
-  /*  if (total1 > total2 && total1 > total3) {
-      $("#France").show();
-      $("#South_Africa").hide();
-      $("#Hawaii").hide();
+   if (total1 > total2 && total1 > total3) {
+      $("#france").show();
+      $("#Sa").hide();
+      $("#hawaii").hide();
     } else if (total2 > total1 && total2 > total3) {
-      $("#Hawaii").show();
-      $("#South_Africa").hide();
-      $("#France").hide();
+      $("#hawaii").show();
+      $("#sa").hide();
+      $("#france").hide();
     } else {
-      $("#South_Africa").show();
-      $("#Hawaii").hide();
-      $("#France").hide();
+      $("#sa").show();
+      $("#hawaii").hide();
+      $("#france").hide();
 
     }
     $("#result").show();
     $(".name").text(name);
-    $(".date").text(date);*/
+    $(".date").text(date);
 
  });
 });
